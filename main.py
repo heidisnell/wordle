22e398690e7cbb1cf0471d4d172864ea46454f32
Heidi Snell <hms8694@gmail.com>
2022-02-27 18:06:38 -0600

    wip

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-class 
+import random
+
+class Wordle:
+    def __init__(self, word_length):
+        self.word_length = word_length
+
+        self.Wordle.new_game()
+
+    def new_game(self, word_length):
+
+
+
+def play(word_length = 5):
+    print("Enter your guess: ")
+
+if __name__ == "__main__":
+    play()
